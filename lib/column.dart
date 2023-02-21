55f2fa05717f099fa2bfb1b462d7fd12a66a8c3a
adilagiska05 <adila_074@smkassalaambandung.sch.id>
2023-02-21 10:30:43 +0700

    latihan flutter

---
+++ lib/column.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+// import 'package:projectmobile/main.dart';
+
+class ColumnWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+     return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text("Nasi Gore + Teh Jeruk Dingin"),
+        Text("MIe Ayam + Teh Lemon Dingin"),
+        Text("Vegetable Fried + Black Coffe"),
+      ],
+     );
+  }
+}
